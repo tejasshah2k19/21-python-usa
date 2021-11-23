869e04e825fd26a81ad628a5aa104ef7fad32674
Tejas <tejas14all@gmail.com>
2021-11-23 07:33:10 +0530

    rev

---
+++ rev/conditionalStatement.py
@@ -0,0 +1,35 @@
+#if
+#if...else
+#if...elif
+
+
+
+#take two numbers from user and find out max from it
+n1 = int(input("Enter number 1"))
+n2 = int(input("Enter number 2"))
+
+
+if n1 > n2:
+    print(n1," is max")
+
+
+if n2 > n1:
+    print(n2," is max")
+
+
+if n1 == n2:
+    print("both are same")
+
+
+#take two numbers from user and ask the operation
+#opertation can be any add,sub,mul
+#as per user's choice perform any operation
+
+
+
+
+
+
+
+
+
---
+++ rev/first.py
@@ -0,0 +1,28 @@
+#take two numbers from user and print it.
+
+"""
+print("Enter two numbers")
+n1 = input()
+n2 = input()
+
+print(n1)
+print(n2)
+"""
+
+#take two numbers from user and perform addition
+
+print("Enter two numbers")
+n1 = int(input())
+n2 = int(input())
+ans = n1 + n2
+print(ans)
+
+
+""" 
+print("Enter two numbers")
+n1 = float(input())
+n2 = float(input())
+ans = n1 + n2
+print(ans)
+
+"""
