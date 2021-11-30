a762c584b8cb56e5ccfcf6d8819267f446a9ca48
Tejas <tejas14all@gmail.com>
2021-11-30 06:45:30 +0530

    loop

---
+++ rev/loop.py
@@ -0,0 +1,62 @@
+#for repeating task we use loop
+
+"""
+
+    while condition:
+        body--logic
+    XXX XXX
+"""
+
+""" 
+i=1
+sum =0
+while i<=5:
+    print("Enter number")
+    num = int(input())
+    sum=sum+num
+    i=i+1
+print("sum = ",sum)
+"""
+
+"""
+    print multiplication table of a number 
+    10 => 
+    10 * 1 = 10
+    ....
+    10 * 10 = 100 
+"""
+
+num = int(input("Enter a number"))
+i=1
+while i<=10 :
+    print(num," * ",i," = ",(num*i))
+    i=i+1
+
+"""
+take starting number and ending number from user and print all the even numbers from it. 
+
+start = 10
+end = 25 
+
+10 12 14 16 18 20 22 24 
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
