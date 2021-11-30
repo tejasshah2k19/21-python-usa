1650484dcbad891a9ad2d0f92ab14b7117e95629
Tejas <tejas14all@gmail.com>
2021-11-30 07:17:37 +0530

    loop

--- rev/loop.py
+++ rev/loop.py
@@ -26,21 +26,74 @@ print("sum = ",sum)
     10 * 10 = 100 
 """
 
+"""
 num = int(input("Enter a number"))
 i=1
 while i<=10 :
     print(num," * ",i," = ",(num*i))
     i=i+1
+"""
+
 
 """
 take starting number and ending number from user and print all the even numbers from it. 
 
-start = 10
-end = 25 
+start = 10 [ read from user] 
+end = 25  [ read from user ] 
 
 10 12 14 16 18 20 22 24 
 
 """
+
+""" 
+start = int(input("enter start num"))
+end = int(input("enter end num"))
+
+i=start
+
+while i<=end:
+    if i%2 == 0:
+        print(i)
+    i=i+1
+
+"""
+
+
+"""
+    take start number and end number from user 
+    start = 10 
+    end = 20 
+    print reverse of all , 20 19 18 17 .. .. . . 10 
+"""
+
+"""
+    foor loop -> for each loop 
+"""
+""" 
+for i in range(1,11):
+    print(i)
+"""
+
+
+
+start = int(input("Enter start number"))
+end = int(input("Enter end number"))
+
+for i in range(start,end+1):
+    print(i)
+
+
+
+
+
+
+"""
+    mulitplication table of a number
+"""
+
+
+
+
 
 
 
